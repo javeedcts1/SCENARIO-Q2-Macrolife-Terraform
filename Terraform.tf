788c4246b20a0a60cd5ceda979f4a0d6d8ebead7
javeedcts1 <85866417+javeedcts1@users.noreply.github.com>
2021-06-14 20:04:03 +0530

    Update Terraform.tf

--- Terraform.tf
+++ Terraform.tf
@@ -30,6 +30,31 @@ variable "VM_username" {
 variable "storageaccountname" {
   type = string
 }
+############################################Defining varibles
+RG_Name = "MacroLife_RG"
+Vnet_Name = "MacroLife-vnet"
+Vnet_addressspace = ["10.0.0.0/16"]
+Timezone = "Pacific Standard Time"
+
+storageaccountname = "macrolifestorageacc"
+
+#####Key vault details are hardcoded in Terraform.tf file
+
+VM_username = "Azureuser"
+server_list  = [
+    {
+        hostname = "server1"
+        SKU = "Standard_DS1_v2"
+		osDiskType = "StandardSSD_LRS"
+		Subnet = ["10.0.1.0/24"]
+    } ,
+    {
+        hostname = "server2"
+        SKU = "Standard_DS1_v2"
+		osDiskType = "StandardSSD_LRS"
+		Subnet = ["10.0.2.0/24"]
+    }
+]
 ##########################################Pulling Password from Key Vault
 
 data "azurerm_key_vault" "keyvault" {
